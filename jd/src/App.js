2492f18adaafa80978795f6400ce1ac4821ade8a
yrq1429 <2421064784@qq.com>
2018-09-04 14:51:03 +0800

    项目开展

---
+++ jd/src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="header-banner"></div>
+        <div className="header-top">头部导航栏</div>
+        <div className="content">
+          <div className="header">
+            
+          </div>
+        </div>
+        
+      </div>
+    );
+  }
+}
+
+export default App;
